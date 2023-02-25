61165f5f8781fb481f87b42023dbe29fc3a66d4b
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2023-02-25 18:35:51 -0400

    dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -95,12 +95,30 @@ router.get('/dashboard', withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
+      include: [{ model: Post }],
     });
 
     const user = userData.get({ plain: true });
-
+console.log(user)
     res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/newpost', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+    res.render('newpost', {
       ...user,
       logged_in: true
     });
